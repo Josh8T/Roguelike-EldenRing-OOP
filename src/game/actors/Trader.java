5c450ab193a5ae6a9c18b2a7135a52b62a8992d9
Aflah Hanif Amarlyadi <aflahamarlyadi@gmail.com>
2023-04-17 15:54:56 +0800

    Create Trader class

---
+++ src/game/actors/Trader.java
@@ -0,0 +1,61 @@
+package game.actors;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actions.DoNothingAction;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.Status;
+
+/**
+ * A class that represents the Trader.
+ * Created by:
+ * @author Aflah Hanif Amarlyadi
+ * Modified by:
+ *
+ */
+public class Trader extends Actor {
+
+    /**
+     * Constructor.
+     *
+     * @param name the name to call the trader in the UI
+     * @param displayChar the character to represent the trader in the UI
+     */
+    public Trader(String name, char displayChar) {
+        super(name, displayChar, 100);
+    }
+
+    /**
+     * The trader cannot move around and does nothing.
+     *
+     * @param actions    collection of possible Actions for this Actor
+     * @param lastAction The Action this Actor took last turn. Can do interesting things in conjunction with Action.getNextAction()
+     * @param map        the map containing the Actor
+     * @param display    the I/O object to which messages may be written
+     * @return the do nothing action
+     */
+    @Override
+    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+        return new DoNothingAction();
+    }
+
+    /**
+     * The trader can trade weapons with any actor that has the HOSTILE_TO_ENEMY capability
+     *
+     * @param otherActor the Actor that might be performing attack
+     * @param direction  String representing the direction of the other Actor
+     * @param map        current GameMap
+     * @return the list of actions the actor can do to the trader
+     */
+    @Override
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        ActionList actions = new ActionList();
+        if(otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)){
+            // TODO: add purchase actions
+            // TODO: add sell actions
+        }
+        return actions;
+    }
+}
