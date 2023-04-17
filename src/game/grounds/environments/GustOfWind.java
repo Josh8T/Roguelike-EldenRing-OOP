10b18029a38b21839d6c7ed2b72000c4824fdf8b
Aflah Hanif Amarlyadi <aflahamarlyadi@gmail.com>
2023-04-17 14:15:26 +0800

    Create GustOfWind class

---
+++ src/game/grounds/environments/GustOfWind.java
@@ -0,0 +1,17 @@
+package game.grounds.environments;
+
+import edu.monash.fit2099.engine.positions.Ground;
+
+/**
+ * A class that represents Gust of Wind, where Lone Wolf spawns.
+ * Created by:
+ * @author Aflah Hanif Amarlyadi
+ * Modified by:
+ *
+ */
+public class GustOfWind extends Ground {
+
+    public GustOfWind() {
+        super('&');
+    }
+}
