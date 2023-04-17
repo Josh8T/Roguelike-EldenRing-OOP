a11f9e3c99ed4fc6f604c533728d80be04e6f0cc
Aflah Hanif Amarlyadi <aflahamarlyadi@gmail.com>
2023-04-17 16:07:36 +0800

    Create GreatKnife class

---
+++ src/game/weapons/GreatKnife.java
@@ -0,0 +1,20 @@
+package game.weapons;
+
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+
+/**
+ * A class that represents the Great Knife weapon.
+ * Created by:
+ * @author Aflah Hanif Amarlyadi
+ * Modified by:
+ *
+ */
+public class GreatKnife extends WeaponItem {
+
+    /**
+     * Constructor.
+     */
+    public GreatKnife() {
+        super("Great Knife", '/', 75, "stabs", 70);
+    }
+}
