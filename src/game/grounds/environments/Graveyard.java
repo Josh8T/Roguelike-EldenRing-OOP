d5e270e8bf530cc18f8c651e7dbcd5e1812a0cde
Aflah Hanif Amarlyadi <aflahamarlyadi@gmail.com>
2023-04-17 14:12:26 +0800

    Create Graveyard class

---
+++ src/game/grounds/environments/Graveyard.java
@@ -0,0 +1,17 @@
+package game.grounds.environments;
+
+import edu.monash.fit2099.engine.positions.Ground;
+
+/**
+ * A class that represents bare dirt.
+ * Created by:
+ * @author Aflah Hanif Amarlyadi
+ * Modified by:
+ *
+ */
+public class Graveyard extends Ground {
+
+    public Graveyard() {
+        super('n');
+    }
+}
