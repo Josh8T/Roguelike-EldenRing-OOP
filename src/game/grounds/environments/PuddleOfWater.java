16229416664ee110eda94da5dc39d00e42028c89
Aflah Hanif Amarlyadi <aflahamarlyadi@gmail.com>
2023-04-17 14:18:03 +0800

    Create PuddleOfWater class

---
+++ src/game/grounds/environments/PuddleOfWater.java
@@ -0,0 +1,17 @@
+package game.grounds.environments;
+
+import edu.monash.fit2099.engine.positions.Ground;
+
+/**
+ * A class that represents Puddle of Water, where Giant Crab and Giant Crayfish spawns.
+ * Created by:
+ * @author Aflah Hanif Amarlyadi
+ * Modified by:
+ *
+ */
+public class PuddleOfWater extends Ground {
+
+    public PuddleOfWater() {
+        super('~');
+    }
+}
