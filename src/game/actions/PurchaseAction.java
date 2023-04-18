b2bca8e53fd17d86f148c7101620e82f59c5ff54
Aflah Hanif Amarlyadi <aflahamarlyadi@gmail.com>
2023-04-18 18:19:58 +0800

    Create PurchaseAction and SellAction abstract classes

---
+++ src/game/actions/PurchaseAction.java
@@ -0,0 +1,13 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+
+/**
+ * A base class that represents the purchase action between actors.
+ * Created by:
+ * @author Aflah Hanif Amarlyadi
+ * Modified by:
+ *
+ */
+public abstract class PurchaseAction extends Action {
+}
---
+++ src/game/actions/SellAction.java
@@ -0,0 +1,13 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+
+/**
+ * A base class that represents the sell action between actors.
+ * Created by:
+ * @author Aflah Hanif Amarlyadi
+ * Modified by:
+ *
+ */
+public abstract class SellAction extends Action {
+}
