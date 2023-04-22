824ac80075375554beab4257dbf8ea85c8fc15a5
Josh8T <joshhernetttan@gmail.com>
2023-04-22 12:43:33 +0800

    Add Giant Dog class

---
+++ src/game/actors/enemies/GiantDog.java
@@ -0,0 +1,25 @@
+package game.actors.enemies;
+
+import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
+import game.behaviours.WanderBehaviour;
+
+/**
+ * A class that represents the Giant Dog enemy.
+ * Created by:
+ * @author Josh Hernett Tan
+ * Modified by:
+ *
+ */
+public class GiantDog extends Enemy {
+    public GiantDog() {
+        super("GiantDog", 'G', 693);
+        this.getBehaviours().put(999, new WanderBehaviour());
+    }
+
+    @Override
+    public IntrinsicWeapon getIntrinsicWeapon() {
+        return new IntrinsicWeapon(314, "headSlam", 90);
+    }
+
+}
+
