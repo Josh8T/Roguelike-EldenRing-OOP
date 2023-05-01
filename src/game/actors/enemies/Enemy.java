7de8303c764d07b0df81023e11b92373a7c07a2e
Josh8T <joshhernetttan@gmail.com>
2023-05-02 00:16:48 +0800

    Fixed follow behaviour on Enemy class to only follow player instead of following player and enemies

--- src/game/actors/enemies/Enemy.java
+++ src/game/actors/enemies/Enemy.java
@@ -40,7 +40,7 @@ public abstract class Enemy extends Actor {
         Location actorLocation = map.locationOf(this);
         for (Exit targetExits : actorLocation.getExits()) {
             Actor target = targetExits.getDestination().getActor();
-            if (target != null) {
+            if (target.hasCapability(Status.HOSTILE_TO_ENEMY)) {
                 this.getBehaviours().put(2, new FollowBehaviour(target));
                 behaviours.remove(3);
             }
