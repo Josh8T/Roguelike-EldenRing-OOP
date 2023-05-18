9dc9dcf782178ecd387775b6476b4b4eccdb861f
dlee0069 <dlee0069@student.monash.edu>
2023-05-18 13:32:05 +0800

    Add documentation for DropsRunes

--- src/game/actors/DropsRunes.java
+++ src/game/actors/DropsRunes.java
@@ -1,5 +1,15 @@
 package game.actors;
 
+/**
+ * Created by:
+ * @author David Lee
+ * Modified by:
+ *
+ */
 public interface DropsRunes {
+    /**
+     * Generates a random value of runes that can be dropped by the actor
+     * @return a random value of runes
+     */
     int generateRuneValue();
 }
