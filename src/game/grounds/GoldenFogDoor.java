3624b03d9340eb61fa77760bcdb1f0584deb4e40
Aflah Hanif Amarlyadi <aflahamarlyadi@gmail.com>
2023-05-21 15:41:09 +0800

    Remove GameMap import in GoldenFogDoor

--- src/game/grounds/GoldenFogDoor.java
+++ src/game/grounds/GoldenFogDoor.java
@@ -2,11 +2,11 @@ package game.grounds;
 
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
-import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
 import game.Destination;
 import game.actions.TravelAction;
+
 /**
  * A class that represents the Golden Fog Door, used to move between maps.
  * Created by:
